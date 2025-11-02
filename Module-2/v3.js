a3bd6ac885b7fe2930b9523339fcd6a6bc6e8747
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-02 15:22:07 +0600

    module 2, vedio 3 done

--- Module-2/v3.js
+++ Module-2/v3.js
@@ -1,1 +1,28 @@
-console.log(true)
+// Sort
+const numbers = [40, 100, 1, 5, 25, 10];
+const fruits = ["apple", "Banana", "cherry", "Date"];
+
+numbers.sort((a, b) => a - b); //a - b means accending order | b - a means desending order
+fruits.sort((a, b) => a.localeCompare(b)); //for mixed lowercase uppdercase
+
+
+// Nested array flat
+const arr = [1, 2, 3, [4, 5, [6, 7, [8, 9]]]]
+const flatArr = arr.flat(); //nested array to flat [default max 1 layer will flat]
+// if number than that times flat | if Infinity than all nested will be flat
+
+const tagsFromPost = [
+    ["JavaScript", "React.js", "CSS3"],
+    ["Node.js", "Express.js"],
+    ["Html5", "CSS3", "React.js"],
+];
+
+const filterTags = [...new Set(tagsFromPost.flat())]; //remove duplicate item, also flat the array and return as an array
+console.log(filterTags)
+
+
+// Some
+const currentUserRoles = ["user", "editor"];
+const featureAccessRoles = ["admin", "manager"];
+
+// Array.from()
---
+++ README.md
@@ -0,0 +1,8 @@
+## Be a Critical Thinker with JS Tentative Outline
+
+| Date | Module | Description |
+| :------- | :------- | :-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+| Oct 30 2025 | `Module-1` | [Mindset over Syntax](https://web.programming-hero.com/level2-batch-6/post/level2-batch-6-text-instructions-module-1) |
+| Nov 01 2025 | `Module-2` | [JavaScript Data Transformation and Aggregation](https://web.programming-hero.com/level2-batch-6/post/level2-batch-6-text-instructions-module-2) |
+| Nov 03 2025 | `Module-3` | [Data Structures that Actually Matter]() |
+| Nov 05 2025 | `Module-4` | [Algorithms in Action]() |
