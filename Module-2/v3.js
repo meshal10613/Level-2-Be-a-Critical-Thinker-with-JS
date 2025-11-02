b361c68b19f31020e5f0563809b724dd98e26f2b
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-02 15:43:20 +0600

    module 2 vedio 4, done

--- Module-2/v3.js
+++ Module-2/v3.js
@@ -1,4 +1,4 @@
-// Sort
+// Sort---------------------------
 const numbers = [40, 100, 1, 5, 25, 10];
 const fruits = ["apple", "Banana", "cherry", "Date"];
 
@@ -6,7 +6,7 @@ numbers.sort((a, b) => a - b); //a - b means accending order | b - a means desen
 fruits.sort((a, b) => a.localeCompare(b)); //for mixed lowercase uppdercase
 
 
-// Nested array flat
+// Nested array flat------------------------
 const arr = [1, 2, 3, [4, 5, [6, 7, [8, 9]]]]
 const flatArr = arr.flat(); //nested array to flat [default max 1 layer will flat]
 // if number than that times flat | if Infinity than all nested will be flat
@@ -18,11 +18,30 @@ const tagsFromPost = [
 ];
 
 const filterTags = [...new Set(tagsFromPost.flat())]; //remove duplicate item, also flat the array and return as an array
-console.log(filterTags)
 
 
-// Some
+// Some---------------------
+const num = [1, 2, 3, 4, 5];
+const hasEvenNum = num.some(n => n % 2 === 0); //if one element has matched the criteria
+// console.log(hasEvenNum)
+
 const currentUserRoles = ["user", "editor"];
 const featureAccessRoles = ["admin", "manager"];
+const canAccess = currentUserRoles.some(role => 
+    featureAccessRoles.includes(role)
+);
+// console.log(canAccess)
 
-// Array.from()
+
+// Array.from()-----------------------
+const array = Array.from({ length: 5 }, (_, i) => i);
+const newA = Array.from([1, 2, 3], (value, i) => value);
+// console.log(newA)
+
+const range = (start, stop, step) => 
+    Array.from(
+        { length: Math.ceil((stop - start) / step) }, 
+        (_, i) => start + i * step  
+    );
+
+console.log(range(1, 11, 1))
