aa420d8b4ec49543d243e5515cf5d4f71f2d9ba8
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-04 19:33:10 +0600

    moduel 3 vedio 3, done!

---
+++ Module-3/v3.js
@@ -0,0 +1,45 @@
+//* Stack Operations LIFO(Last In First Out) -------------
+//? push() - add to the last
+//? pop() - remove the last
+//? peek() - value of the last item
+
+class Stack {
+    constructor () {
+        this.items = [];
+    };
+
+    //* O(1)
+    push (value) { //? add an item in the last to the array
+        this.items.push(value);
+    };
+
+    pop (value) { //? remove the last element from the array
+        if(this.isEmpty()){
+            return undefined;
+        };
+        return this.items.pop(value);
+    };
+
+    peek () { //? get the last item value from the array
+        if(this.isEmpty()){
+            return undefined;
+        };
+        return this.items[ this.items.length - 1 ];
+    };
+
+    isEmpty () { //? check that the array is empty or not
+        return this.items.length === 0;
+    };
+
+    print () {
+        console.log(this.items)
+    }
+};
+
+const stack = new Stack();
+stack.push(10);
+stack.push(20);
+
+console.log(stack.peek());
+stack.print();
+console.log(stack.pop());
