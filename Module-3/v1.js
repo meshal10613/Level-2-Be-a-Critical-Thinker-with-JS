1252abbf4a9a4b9e5cfa2ed7b86345f49d6bd282
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-04 13:51:52 +0600

    module 3 vedio 1, done!

---
+++ Module-3/v1.js
@@ -0,0 +1,25 @@
+//* Stateless vs Statefull
+// const counter = (ammount) => {
+//     let count = 0;
+
+//     count = count + ammount;
+//     return count;
+// };
+
+// console.log(counter(3));
+// console.log(counter(2));
+
+const counter = {
+    count: 0,
+    add(ammount) {
+        this.count = this.count + ammount;
+    },
+
+    print(){
+        console.log(console.log(this.count))
+    },
+};
+
+counter.add(2);
+counter.add(3);
+counter.print();
