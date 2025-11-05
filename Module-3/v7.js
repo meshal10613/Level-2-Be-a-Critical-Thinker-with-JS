c198c9deb9b7c7e36c97358268e9c03e028bf956
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-05 20:02:18 +0600

    module 3 vedio 8 done

--- Module-3/v7.js
+++ Module-3/v7.js
@@ -23,15 +23,53 @@ class LinkedList {
             this.tail.next = newNode;
             this.tail = newNode;
         };
+
         this.length ++;
+        return this;
     };
 
-    prepend () { //? add to the first in the linkedlist
+    prepend (value) { //? add to the first in the linkedlist
+        const newNode = new Node(value);
+        // If the linkedlist is empty
+        if(this.head === null){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // If the linkedlist is not empty
+            newNode.next = this.head;
+            this.head = newNode;
+        };
 
+        this.length ++;
+        return this;
     };
 
-    insert () {
+    insert (index, value) {
+        if (index < 0 || index > this.length){
+            console.error("Index out of bound!");
+            return undefined;
+        };
 
+        // If insert in in the start of the linkedlist
+        if (index === 0) {
+            return this.prepend(value);
+        };
+
+        // If insert in in the end of the linkedlist
+        if (index === this.length) {
+            return this.append(value);
+        };
+
+        // If insert in in the middle of the linkedlist
+        // find the leading node
+        let count = 0;
+        let leadingNode = this.head;
+
+        while( count !== index ) {
+            leadingNode = leadingNode.next;
+            count ++;
+        };
+        console.log(leadingNode)
     };
 
     remove () {
@@ -53,5 +91,8 @@ const linkedList = new LinkedList();
 linkedList.append(1);
 linkedList.append(2);
 linkedList.append(3);
+
+linkedList.prepend(4);
+linkedList.insert(1, 5);
 
 linkedList.print();
