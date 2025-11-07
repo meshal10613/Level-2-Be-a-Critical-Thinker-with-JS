85b928dc5ac795ac764e78f84f6f824fe8c94fcf
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-07 19:44:42 +0600

    module-4 vedio 9, done!

---
+++ Module-4/v9.js
@@ -0,0 +1,26 @@
+//* Selection Sort
+
+const selectionSort = (arr) => {
+	for (let i = 0; i < arr.length; i++) {
+		let minIndex = i;
+		for(let j = i + 1; j < arr.length; j++) {
+			if(arr[j] < arr[minIndex]) {
+				minIndex = j;
+			};
+		};
+
+		if(minIndex !== i) {
+			//? Swaping
+			// let tem = arr[i];
+			// arr[i] = arr[minIndex];
+			// arr[minIndex] = tem;
+
+			[arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+		};
+	};
+	return arr;
+};
+
+const sortedArray = selectionSort([5, 3, 8, 4, 2]);
+console.log(sortedArray);
+//? Expected Output- [ 2, 3, 4, 5, 8 ]
